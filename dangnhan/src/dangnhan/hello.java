219ed6e22453c6cbc38cd13741ecd74f1bd9d975
nhanmmo2005 <hopdangnhan@dtu.edu.vn>
2025-04-08 10:00:55 +0700

    update chức năng mới

--- dangnhan/src/dangnhan/hello.java
+++ dangnhan/src/dangnhan/hello.java
@@ -1,10 +1,16 @@
 package dangnhan;
 
+import java.util.Scanner;
+
 public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello . XIn chao ban. Test tiep nha");
+		System.out.println("Nhập tên của bạn: ");
+		Scanner nhap = new Scanner(System.in);
+		String a = nhap.nextLine();
+		System.out.println("Chào, "+a);
+
 	}
 
 }

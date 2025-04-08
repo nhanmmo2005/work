075cec985298ac7d165ff77bb49886e4e6c3e7a2
nhanmmo2005 <hopdangnhan@dtu.edu.vn>
2025-04-08 10:05:56 +0700

    ok

--- dangnhan/src/dangnhan/hello.java
+++ dangnhan/src/dangnhan/hello.java
@@ -3,7 +3,6 @@ package dangnhan;
 import java.util.Scanner;
 
 public class hello {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Nhập tên của bạn: ");
